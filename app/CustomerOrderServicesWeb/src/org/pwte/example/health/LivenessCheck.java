f2e9358735a39c5134907e15ae62ba8cb16ad122
Artur Dzmitryieu <dzmitry@ca.ibm.com>
2020-04-16 11:05:49 -0400

    Update the health probes

--- app/CustomerOrderServicesWeb/src/org/pwte/example/health/LivenessCheck.java
+++ app/CustomerOrderServicesWeb/src/org/pwte/example/health/LivenessCheck.java
@@ -1,5 +1,9 @@
 package org.pwte.example.health;
 
+import java.lang.management.ManagementFactory;
+import java.lang.management.ThreadInfo;
+import java.lang.management.ThreadMXBean;
+
 import javax.enterprise.context.ApplicationScoped;
 
 import org.eclipse.microprofile.health.HealthCheck;
@@ -12,7 +16,22 @@ public class LivenessCheck implements HealthCheck {
 
 	@Override
 	public HealthCheckResponse call() {
+		ThreadMXBean tBean = ManagementFactory.getThreadMXBean();
+		long ids[] = tBean.findMonitorDeadlockedThreads();
+		if (ids !=null) {
+			ThreadInfo threadInfos[] = tBean.getThreadInfo(ids);
+			for (ThreadInfo ti : threadInfos) {
+				double seconds = ti.getBlockedTime() / 1000.0;
+				if (seconds > 60) {
+					return HealthCheckResponse.named("Liveness").down().build();
+				}
+			}
+		}
 		return HealthCheckResponse.named("Liveness").up().build();
 	}
 
 }
+
+
+	
+
--- app/CustomerOrderServicesWeb/src/org/pwte/example/health/ReadinessCheck.java
+++ app/CustomerOrderServicesWeb/src/org/pwte/example/health/ReadinessCheck.java
@@ -1,16 +1,47 @@
 package org.pwte.example.health;
 
+import org.eclipse.microprofile.config.inject.ConfigProperty;
 import org.eclipse.microprofile.health.HealthCheck;
 import org.eclipse.microprofile.health.HealthCheckResponse;
 import org.eclipse.microprofile.health.Readiness;
+
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
 
 @ApplicationScoped
 @Readiness
 public class ReadinessCheck implements HealthCheck {
 
+	@Inject
+	@ConfigProperty(name="SSO_URI")
+	private String keycloakURI;
+	
 	@Override
 	public HealthCheckResponse call() {
+		HttpURLConnection con = null;
+		try {
+			
+			URL url = new URL(keycloakURI);
+			con = (HttpURLConnection) url.openConnection();
+			con.setRequestMethod("GET");
+			int status = con.getResponseCode();
+			System.out.println("Status:" + status);
+			if (status != 200) {
+				return HealthCheckResponse.named("Readiness").down().build();
+			}
+
+		} catch (IOException e) {
+			System.out.println("Exception:" + e);
+			return HealthCheckResponse.named("Readiness").down().build();
+
+		}
+		finally {
+			con.disconnect();
+		}
 		return HealthCheckResponse.named("Readiness").up().build();
 	}
 
